bb69e835f113dfc69f6b525010a9fa075793ec33
project-faceTrace <157962205+project-faceTrace@users.noreply.github.com>
2024-03-05 22:09:41 +0530

    Delete hod_page directory

--- hod_page/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from. import views
-urlpatterns = [
-    path('',views.hod_page,name="hod_page"), 
-    path('hodsignout',views.hodsignout,name="hodsignout"),
-]
